8373bedddbc5e2b94fb0ffe0351f3f184043ecdf
Yari <narducciyarigmail.com>
2023-02-13 23:42:32 +0100

    creazione relazione hastoone

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Person extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'name',
+        'mail'
+    ];
+   public function personDetail(){
+    return $this ->hasone(PersonDetail::class);
+   }
 }
